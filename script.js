c055fff05f632b35f11bc376434767bb23a40e25
vikrantkharey <110925298+vikrantkharey@users.noreply.github.com>
2022-08-09 21:50:32 +0530

    java Script

---
+++ script.js
@@ -0,0 +1,53 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+$(document).ready(function () {
+
+  $('#menu').click(function () {
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load', function () {
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if ($(window).scrollTop() > 0) {
+      $('.top').show();
+    } else {
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click', function (e) {
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop: $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
